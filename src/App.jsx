3d9c55b57f610587c4725b69953e6ee64e0d0801
ashwani <ashwani.singh@daffodilsw.com>
2022-06-19 17:25:27 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, {Suspense} from "react";
+import Header from "modules/Header";
+
+const App = () => {
+  return (
+    <div>
+        <Header title="Modules 2" />
+    </div>)
+}
+
+
+export default App;
